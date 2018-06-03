6610cb9dc1f3072122b824b53a21eed93568a403
jose51197 <jose51197@hotmail.com>
2018-06-03 14:57:50 -0600

    Metodo insert y registro

--- app/src/main/java/com/example/jose5/a35mm/Conexion.java
+++ app/src/main/java/com/example/jose5/a35mm/Conexion.java
@@ -1,6 +1,5 @@
 package com.example.jose5.a35mm;
 
-import android.text.Editable;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -12,9 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -39,7 +36,6 @@ public class Conexion {
             httpConn.connect();
 
             InputStream is = httpConn.getInputStream();
-            is.toString();
             queryResult = convertinputStreamToString(is);
             return new JSONArray(queryResult);
 
@@ -49,27 +45,28 @@ public class Conexion {
         }
     }
 
-    public void AddMovie(String name, String description, String image, String year) throws IOException {
-        String link="https://proyectoreque.000webhostapp.com/connect/movie.php";
-        String data  = URLEncoder.encode("description", "UTF-8") + "=" +
-                URLEncoder.encode(description, "UTF-8");
-        data += "&" + URLEncoder.encode("name", "UTF-8") + "=" +
-                URLEncoder.encode(name, "UTF-8");
-        data += "&" + URLEncoder.encode("year", "UTF-8") + "=" +
-                URLEncoder.encode(year, "UTF-8");
-        data += "&" + URLEncoder.encode("url", "UTF-8") + "=" +
-                URLEncoder.encode(image, "UTF-8");
 
-        URL reqURL = new URL(link); //the URL we will send the request to
-        HttpURLConnection connection = (HttpURLConnection) reqURL.openConnection();
-        String post = data;
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.connect();
-        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream()); //we will write our request data here
-        writer.write(post);
-        writer.flush();
-        writer.close();
+    public boolean Insert(String table,String columns, String values) {
+        StringBuilder result = new StringBuilder();
+        try {
+            //digamos que 1,2,3 cambia a '+1+','+2+','+3+'
+            values="'"+values.replace(",","','")+"'";
+            URL url = new URL("https://proyectoreque.000webhostapp.com/connect/insert.php?table=" + table + "&columns="+columns +"&values="+values);
+            Log.println(Log.DEBUG, "url", url.toString());
+            URLConnection conn = url.openConnection();
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            InputStream is = httpConn.getInputStream();
+            Log.println(Log.DEBUG,"insert",convertinputStreamToString(is));
+            return convertinputStreamToString(is).equals("inserted");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public static String convertinputStreamToString(InputStream ists) throws IOException {
--- app/src/main/java/com/example/jose5/a35mm/Register.java
+++ app/src/main/java/com/example/jose5/a35mm/Register.java
@@ -1,27 +1,92 @@
 package com.example.jose5.a35mm;
 
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jose5.a35mm.R;
 
-public class Register extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
 
+public class Register extends AppCompatActivity  {
+    private RegisterTask mRegTask = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+        Button regButton = (Button) findViewById(R.id.registerButton);
+        regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 register();
             }
         });
     }
+
     private void register(){
-        //logica del registro, mañana
+        if (mRegTask!= null) {
+            return;
+        }
+        EditText pass = (EditText) findViewById(R.id.rpass);
+        EditText user = (EditText) findViewById(R.id.ruser);
+        EditText nombre = (EditText) findViewById(R.id.rnombre);
+        EditText email = (EditText) findViewById(R.id.remail);
+        mRegTask= new RegisterTask(email.getText().toString(),pass.getText().toString(),user.getText().toString(),nombre.getText().toString());
+        mRegTask.execute((Void) null);
+
+    }
+
+    public class RegisterTask extends AsyncTask<Void, Void, Boolean> {
+
+        private String mEmail;
+        private String mPassword;
+        private String mUser;
+        private String mName;
+
+        RegisterTask(String email, String password,String user,String name) {
+            mEmail = email;
+            mPassword = password;
+            mName=name;
+            mUser=user;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            Conexion c = new Conexion();
+            boolean registro = c.Insert("Users","Nombre,User,Pass,email",mName+","+mUser+","+mPassword+","+mEmail);
+            //boolean registro = c.Insert("prueba","p,p2","6,7");
+            return registro;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mRegTask = null;
+            if (success) {
+                finish();
+                notificate("Registrado");
+
+            } else {
+                notificate("Error de conexion/datos");
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mRegTask = null;
+        }
+
+        private void notificate(String notification){
+            Toast.makeText(getApplicationContext(), notification, Toast.LENGTH_LONG).show();
+        }
     }
 }
